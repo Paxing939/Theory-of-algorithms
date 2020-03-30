60b0e602df36dcc7e58cc41787bf304838ec09b5
Ilya Sachek <isachek2001@gmai.com>
2020-03-30 08:36:07 +0300

    Created first algorithm for microchip task

--- cmake-build-debug/CMakeFiles/TA.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/TA.dir/DependInfo.cmake
@@ -4,12 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-<<<<<<< HEAD
-  "/home/ilya/CLionProjects/Theory-of-algorithms/archive.cpp" "/home/ilya/CLionProjects/Theory-of-algorithms/cmake-build-debug/CMakeFiles/TA.dir/archive.cpp.o"
   "/home/ilya/CLionProjects/Theory-of-algorithms/microchip.cpp" "/home/ilya/CLionProjects/Theory-of-algorithms/cmake-build-debug/CMakeFiles/TA.dir/microchip.cpp.o"
-=======
-  "C:/Users/Lenovo/CLionProjects/TA/microchip.cpp" "C:/Users/Lenovo/CLionProjects/TA/cmake-build-debug/CMakeFiles/TA.dir/microchip.cpp.obj"
->>>>>>> 43f4f4c7b0417cd690e8b43d0b74b0fa8ba9d481
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/TA.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/TA.dir/cmake_clean.cmake
@@ -1,15 +1,7 @@
 file(REMOVE_RECURSE
-<<<<<<< HEAD
-  "CMakeFiles/TA.dir/archive.cpp.o"
   "CMakeFiles/TA.dir/microchip.cpp.o"
   "TA"
-=======
-  "CMakeFiles/TA.dir/microchip.cpp.obj"
-  "TA.exe"
-  "TA.exe.manifest"
->>>>>>> 43f4f4c7b0417cd690e8b43d0b74b0fa8ba9d481
   "TA.pdb"
-  "libTA.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
