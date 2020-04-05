238aee40b6220036342bf5d5be1a7e9864c45c62
Ilya Sachek <isachek2001@gmail.com>
2020-04-06 00:20:12 +0300

    Made input and output for Dijkstra algorithm

--- dijkstra_algorithm.cpp
+++ dijkstra_algorithm.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
-
-const int INF = 1000000000;
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
 int main() {
-  int n;
-  ... чтение n ...
-  vector < vector < pair<int,int> > > g (n);
-  ... чтение графа ...
-  int s = ...; // стартовая вершина
+  std::ofstream writer("output.txt");
+  std::ifstream reader("input.txt");
+  int n, m;
+  reader >> n >> m;
 
-  vector<int> d (n, INF),  p (n);
+  int index, vertex, length;
+  std::vector<std::vector<std::pair<int, int>>> graph(n);
+  for (int i = 0; i < n; ++i) {
+    reader >> index >> vertex >> length;
+    graph[index].push_back({vertex, length});
+  }
+
+  int s = 0;
+  std::vector<int> d(n, INT_MAX), p(n);
   d[s] = 0;
-  vector<char> u (n);
-  for (int i=0; i<n; ++i) {
+  std::vector<bool> used(n);
+  for (int i = 0; i < n; ++i) {
     int v = -1;
-    for (int j=0; j<n; ++j)
-      if (!u[j] && (v == -1 || d[j] < d[v]))
+    for (int j = 0; j < n; ++j) {
+      if (!used[j] && (v == -1 || d[j] < d[v])) {
         v = j;
-    if (d[v] == INF)
-      break;
-    u[v] = true;
+      }
+    }
 
-    for (size_t j=0; j<g[v].size(); ++j) {
-      int to = g[v][j].first,
-              len = g[v][j].second;
+    if (d[v] == INT_MAX) {
+      break;
+    }
+    used[v] = true;
+
+    for (size_t j = 0; j < graph[v].size(); ++j) {
+      int to = graph[v][j].first, len = graph[v][j].second;
+
       if (d[v] + len < d[to]) {
         d[to] = d[v] + len;
         p[to] = v;
       }
     }
   }
+
+  writer << *std::min_element(d.begin(), d.end());
 }
