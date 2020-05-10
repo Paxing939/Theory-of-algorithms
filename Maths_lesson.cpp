3f576c7ad5a6dd6fc0f6c3d6e8165ecbada4798b
Ilya Sachok <isachek2001@gmail.com>
2020-05-10 22:25:08 +0300

    Regular heap doesn't work, still have some wrong answers and lack of memory

--- Maths_lesson.cpp
+++ Maths_lesson.cpp
@@ -4,137 +4,180 @@
 #include <sstream>
 #include <ctime>
 #include <algorithm>
-#include <bits/stdc++.h>
+
+bool Comparator(int a, int b) {
+	return a > b;
+}
+
+class Heap {
+public:
+
+	void push(int value) {
+		int i = heap.size() - 1;
+
+		while (i > 0) {
+			int j = (i - 1) / 2;
+			if (heap[j].first < heap[i].first) {
+				break;
+			}
+			if (heap[j].first == heap[i].first) {
+
+				break;
+			}
+			std::swap(heap[j], heap[i]);
+			i = j;
+		}
+	}
+
+	void pop() {
+
+	}
+
+	int top() {
+		return heap[0].first;
+	}
+
+private:
+	std::vector<std::pair<int, int>> heap;
+};
 
 std::string MathsLesson(const std::string &test) {
-  std::stringstream ss(test), out;
+	std::stringstream ss(test), out;
 
-  int n;
-  ss >> n;
+	int n;
+	ss >> n;
 
-  std::vector<int> numbers(n), sums_to_skip(200 * n);
-  int current = 0, fill = 0;
-  int i = 0, j = 0, sum = 0;
-  ss >> sum;
-  numbers[j] = sum / 2;
-  out << numbers[j] << "\n";
-  i++, j++;
-  while (i < n * n) {
-    if (j == n) {
-      break;
-    }
+	std::vector<int> numbers(n), sums_to_skip(200 * n);
+	int current = 0, fill = 0;
+	int i = 0, j = 0, sum = 0;
+	ss >> sum;
+	numbers[j] = sum / 2;
+	out << numbers[j] << "\n";
+	i++, j++;
+	while (i < n * n) {
+		if (j == n) {
+			break;
+		}
 
-    ss >> sum;
-    for (int k = current; k < fill; ++k) {
-      if (sum == sums_to_skip[k]) {
-        ss >> sum;
-        current++;
-      }
-    }
+		ss >> sum;
+		for (int k = current; k < fill; ++k) {
+			if (sum == sums_to_skip[k]) {
+				ss >> sum;
+				current++;
+			}
+		}
 
-    numbers[j] = sum - numbers[0];
-    out << numbers[j] << '\n';
-    j++;
-    for (int k = 0; k < j; ++k) {
-      sums_to_skip[fill] = numbers[j - 1] + numbers[k];
-      fill++;
-    }
+		numbers[j] = sum - numbers[0];
+		out << numbers[j] << '\n';
+		j++;
+		for (int k = 0; k < j; ++k) {
+			sums_to_skip[fill] = numbers[j - 1] + numbers[k];
+			fill++;
+		}
 
-    i++;
-  }
+		i++;
+	}
 
-  return out.str();
+	return out.str();
 }
 
 void MathsLesson() {
-  int n;
-  std::cin >> n;
+	int n;
+	std::cin >> n;
 
-  std::vector<int> numbers(n);
-  std::priority_queue<int, std::vector<int>, std::greater<>> heap;
+	std::vector<int> numbers(n);
+	std::vector<int> heap;
 
-  int i = 0, j = 0, sum = 0;
-  std::cin >> sum;
-  numbers[j] = sum / 2;
-  std::cout << numbers[j] << "\n";
-  i++, j++;
-  while (i < n * n) {
-    if (j == n) {
-      break;
-    }
+	int i = 0, j = 0, sum = 0;
+	std::cin >> sum;
+	numbers[j] = sum / 2;
+	std::cout << numbers[j] << "\n";
+	i++, j++;
+	while (i < n * n) {
+		if (j == n) {
+			break;
+		}
 
-    std::cin >> sum;
-    while (true) {
-      if (!heap.empty()) {
-        if (sum == heap.top()) {
-          std::cin >> sum;
-          heap.pop();
-        } else {
-          break;
-        }
-      } else {
-        break;
-      }
-    }
+		std::cin >> sum;
+		while (true) {
+			if (!heap.empty()) {
+				if (sum == heap[0]) {
+					std::cin >> sum;
+					pop_heap(heap.begin(), heap.end(), Comparator);
+					heap.pop_back();
+				} else {
+					if (sum > heap[0]) {
+						pop_heap(heap.begin(), heap.end(), Comparator);
+						heap.pop_back();
+					} else {
+						break;
+					}
+				}
+			} else {
+				break;
+			}
+		}
 
-    numbers[j] = sum - numbers[0];
-    std::cout << numbers[j] << '\n';
-    j++;
-    for (int k = 0; k < j; ++k) {
-      heap.push(numbers[j - 1] + numbers[k]);
-      if (k != j - 1) {
-        heap.push(numbers[k] + numbers[j - 1]);
-      }
-    }
+		numbers[j] = sum - numbers[0];
+		std::cout << numbers[j] << '\n';
+		j++;
+		for (int k = 0; k < j; ++k) {
+			heap.push_back(numbers[j - 1] + numbers[k]);
+			push_heap(heap.begin(), heap.end(), Comparator);
+			if (k != j - 1) {
+				heap.push_back(numbers[k] + numbers[j - 1]);
+				push_heap(heap.begin(), heap.end(), Comparator);
+			}
+		}
 
-    i++;
-  }
+		i++;
+	}
 }
 
 void Generator() {
-  int n = 5, max_number = 5;
-  srand(time(0));
+	int n = 5, max_number = 5;
+	srand(time(0));
 
-  for (int i = 0; i < 100'000; ++i) {
-    std::stringstream ss, true_answer;
+	for (int i = 0; i < 100'000; ++i) {
+		std::stringstream ss, true_answer;
 
-    std::vector<int> numbers(n), sums(n * n);
-    for (int j = 0; j < n; ++j) {
-      numbers[j] = rand() % max_number + 1;
-    }
+		std::vector<int> numbers(n), sums(n * n);
+		for (int j = 0; j < n; ++j) {
+			numbers[j] = rand() % max_number + 1;
+		}
 
-    std::sort(numbers.begin(), numbers.end());
-    for (int j = 0; j < n; ++j) {
-      true_answer << numbers[j] << '\n';
-    }
+		std::sort(numbers.begin(), numbers.end());
+		for (int j = 0; j < n; ++j) {
+			true_answer << numbers[j] << '\n';
+		}
 
-    ss << n << '\n';
-    int ind = 0;
-    for (int j = 0; j < n; ++j) {
-      for (int k = 0; k < n; ++k) {
-        sums[ind++] = numbers[j] + numbers[k];
-      }
-    }
-    std::sort(sums.begin(), sums.end());
-    for (int j = 0; j < n * n; ++j) {
-      ss << sums[j] << ' ';
-    }
+		ss << n << '\n';
+		int ind = 0;
+		for (int j = 0; j < n; ++j) {
+			for (int k = 0; k < n; ++k) {
+				sums[ind++] = numbers[j] + numbers[k];
+			}
+		}
+		std::sort(sums.begin(), sums.end());
+		for (int j = 0; j < n * n; ++j) {
+			ss << sums[j] << ' ';
+		}
 
-    std::string algorithm = MathsLesson(ss.str());
+		std::string algorithm = MathsLesson(ss.str());
 
-    if (true_answer.str() == algorithm) {
-      std::cout << "Test #" << i + 1 << " completed successfully!\n";
-    } else {
-      std::cout << "Test #" << i + 1 << " failed!\n";
-      std::ofstream writer("log.txt");
-      writer << algorithm;
-      writer << "-------------------------\n";
-      writer << true_answer.str();
-      writer << "-------------------------\n";
-      writer << ss.str();
-      return;
-    }
-  }
+		if (true_answer.str() == algorithm) {
+			std::cout << "Test #" << i + 1 << " completed successfully!\n";
+		} else {
+			std::cout << "Test #" << i + 1 << " failed!\n";
+			std::ofstream writer("log.txt");
+			writer << algorithm;
+			writer << "-------------------------\n";
+			writer << true_answer.str();
+			writer << "-------------------------\n";
+			writer << ss.str();
+			return;
+		}
+	}
 }
 
 int main() {
@@ -158,5 +201,5 @@ int main() {
 //    pq.pop();
 //  }
 
-  return 0;
+	return 0;
 }
