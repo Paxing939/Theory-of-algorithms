40e2587eb91a40b1a41010d248488c763b11a805
Ilya Sachok <isachek2001@gmail.com>
2020-05-25 20:19:47 +0300

    15/17 tests complete

--- Ali-baba.cpp
+++ Ali-baba.cpp
@@ -2,89 +2,46 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <utility>
-#include <iomanip>
 #include <iostream>
 
-struct Pair {
-	int first;
-	int second;
-};
-
-void PrintMatrix(const std::vector<std::vector<Pair>>& matrix) {
-	for (const auto& vec : matrix) {
-		for (const auto& x_ : vec) {
-			std::cout << "(" << x_.first << ", " << x_.second << ") ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-//std::ostream operator<<(std::ostream out, Pair pair) {
-//	out << "(" << pair.first << ", " << pair.second << ") ";
-//}
-
 int main() {
-	std::ifstream reader("input.txt");
-	std::ofstream writer("output.txt");
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
 
-	int64_t infinity = std::numeric_limits<int32_t>::max() - 10;
-	int64_t n;
-	reader >> n;
+	int64_t infinity = std::numeric_limits<int32_t>::max(), n;
+	std::cin >> n;
 
 	std::vector<int64_t> positions(n), time(n);
-	std::vector<std::vector<Pair>> dp(n + 1, std::vector<Pair>(n));
+	std::vector<std::vector<std::pair<int64_t, int64_t>>> dp(n, std::vector<std::pair<int64_t, int64_t>>(n));
 
 	for (int64_t i = 0; i < n; ++i) {
-		reader >> positions[i] >> time[i];
+		std::cin >> positions[i] >> time[i];
 	}
 
-//	for (int64_t k = 2; k < n * 2 + 2; k += 2) {
-//		for (int64_t i = 0; i < n - k / 2; ++i) {
-//			int64_t first = dp[k - 2][i] + positions[i + k / 2] - positions[i + k / 2 - 1];
-//			int64_t second = dp[k - 1][i] + positions[i + k / 2] - positions[i];
-//
-//			dp[k][i] = std::min(first, second);
-//			if (dp[k][i] > time[i + k / 2]) {
-//				dp[k][i] = infinity;
-//			}
-//
-//			first = dp[k - 2][i + 1] + positions[i + k / 2] - positions[i];
-//			second = dp[k - 1][i + 1] + positions[i + 1] - positions[i];
-//
-//			dp[k + 1][i] = std::min(first, second);
-//			if (dp[k + 1][i] > time[i]) {
-//				dp[k + 1][i] = infinity;
-//			}
-//		}
-//	}
-
 	for (int64_t k = 1; k < n; k++) {
-		for (int64_t i = 0; i < n - k; ++i) {
-			int64_t first = dp[k - 1][i].first + positions[i + k] - positions[i + k - 1];
-			int64_t second = dp[k - 1][i].second + positions[i + k] - positions[i];
+		for (int64_t i = 0; i < n - k; i++) {
 
-			dp[k][i].first = std::min(first, second);
+			dp[k][i].first = std::min(dp[k - 1][i].first + positions[i + k] - positions[i + k - 1],
+			                          dp[k - 1][i].second + positions[i + k] - positions[i]);
 			if (dp[k][i].first > time[i + k]) {
 				dp[k][i].first = infinity;
 			}
 
-			first = dp[k - 1][i + 1].first + positions[i + k] - positions[i];
-			second = dp[k - 1][i + 1].second + positions[i + 1] - positions[i];
-
-			dp[k + 1][i].second = std::min(first, second);
-			if (dp[k + 1][i].second > time[i]) {
-				dp[k + 1][i].second = infinity;
+			dp[k][i].second = std::min(dp[k - 1][i + 1].first + positions[i + k] - positions[i],
+			                           dp[k - 1][i + 1].second + positions[i + 1] - positions[i]);
+			if (dp[k][i].second > time[i]) {
+				dp[k][i].second = infinity;
 			}
 		}
 	}
 
-	PrintMatrix(dp);
-	int64_t result = std::min(dp[n - 1][0].first, dp[n - 2][0].second);
+	int64_t result = std::min(dp[n - 1][0].first, dp[n - 1][0].second);
 	if (result != infinity) {
-		writer << result;
+		std::cout << result;
 	} else {
-		writer << "No solution";
+		std::cout << "No solution";
 	}
 
 	return 0;
